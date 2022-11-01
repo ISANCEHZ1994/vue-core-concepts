9e470653dc5a61e16384733a258f8bd2a191a348
Israel Sanchez <Israel.pro1994@gmail.com>
2022-11-01 13:59:24 -0500

    learning Vue

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    // data name NOT up to you you MUST have it
+    // data() MUST ALWAYS return an OBJECT
+    data(){
+        return {
+            courseGoal: 'Finish the course and learn Vue!',
+        }
+    }
+});
+
+// this means that Vue will make the information above available to the selector (in this case ID) user-goal
+// again only the area where the id name user-goal will be able to display the text from coureGoal variable
+app.mount('#user-goal');
