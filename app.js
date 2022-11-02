cd7909f25cda375d6cb5030276e10d590571d8f2
Israel Sanchez <Israel.pro1994@gmail.com>
2022-11-01 19:49:07 -0500

    we can now see the differences when using v-html property

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const app = Vue.createApp({
     // data name NOT up to you you MUST have it
-    // data() MUST ALWAYS return an OBJECT
+    // data() MUST ALWAYS return an OBJECT because data itself is a function
     data(){
         return {
             courseGoal: 'Finish the course and learn Vue!',
+            courseGoalA: 'Refer to original courseGoal above!',
+            courseGoalB: '<h2>Master Vue and build amazing apps!</h2>',
             vueLink: 'https://vuejs.org/'
-        }
+        };
+    },
+    // is an object where you can define functions
+    methods: {
+        firstFunction(){
+            const randomNum = Math.random();
+            // return randomNum < 0.5 ? "Learn Vue" : "Master Vue" 
+            // as weird as it seems Vue understand the variables inside data function so we can use the this. keyword
+            return randomNum < 0.5 ? this.courseGoal : this.courseGoalB
+        },
+        // just so we can see the difference of what the v-html proerty does inside of a tag inside of HTML
+        // notice how we added the <h2> tags above inside of the courseGoalB - the html file will not be able to read it without the property
+        vhtmlFunction(){
+            const randomNum = Math.random();
+            return randomNum < 0.5 ? this.courseGoalA : this.courseGoalB
+        },
     }
 });
 
